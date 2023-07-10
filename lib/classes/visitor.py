2752b573209efa480f6abd132303c69b5c20e832
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-07-10 10:36:18 -0400

    merge with me

--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -1,8 +1,22 @@
+import ipdb
+
 class Visitor:
 
-    def __init__(self, name):
+    def __init__(self, name="Visitor"):
         self.name = name
-        
+        print(name)
+
+    def get_name(self): 
+        return self._name
+
+    def set_name(self, name):
+        if name is str:
+            self._name = name
+        else: 
+            raise Exception("name does not fit criteria")
+
+    name = property(get_name , set_name)
+
     def trips(self, new_trip=None):
         from classes.trip import Trip
         pass
@@ -10,3 +24,5 @@ class Visitor:
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
         pass
+
+visitor_one = Visitor("Bob")
